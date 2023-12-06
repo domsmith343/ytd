a33cfe35b38e18d46fe346e009024ee8079a1296
Dom <79632486+domsmith343@users.noreply.github.com>
2023-12-06 11:49:44 -0800

    Add files via upload

---
+++ youtube.py
@@ -0,0 +1,19 @@
+from pytube import YouTube
+import tkinter as tk
+from tkinter import filedialog
+
+def download_video(url, save_path):
+    try:
+        yt = YouTube(url)
+        streams = yt.streams.filter(progressive=True, file_extension='mp4')
+        highest_res = streams.get_highest_resolution()
+        highest_res.download(output_path=save_path)  # Fix: Use highest_res instead of highest_res_stream
+        print("Download completed!")
+        
+    except Exception as e:
+        print(e)
+        
+url = "https://youtu.be/1xCkl9ZcQp4?si=Uztp_t6PszYXS3Ly"
+save_path = "users/dom/downloads"
+
+download_video(url, save_path)
